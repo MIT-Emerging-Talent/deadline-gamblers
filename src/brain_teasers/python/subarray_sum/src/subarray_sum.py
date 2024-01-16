8edc23c84f3ced63a1f8fb6f3231d8e2478ee50d
fahim <fyousufzai93@gmail.com>
2024-01-16 20:04:19 +0430

    python function to sum up the elements of a sub-array

---
+++ src/brain_teasers/python/subarray_sum/src/subarray_sum.py
@@ -0,0 +1,23 @@
+def subarray_sum(arr, start_index, end_index):
+    """
+    Calculate the sum of a sub-array from a given array
+
+    Parameters:
+    - arr (list): Input list of integers.
+    - start_index: Start index of the sub-array
+    - end_index: End index of the sub-array
+
+    Returns an integer representing sum of the sub-array
+    """
+    
+    sub_array = arr[start_index:end_index]
+    total = sum(sub_array)
+    return total
+
+
+# checks
+# print(subarray_sum([0, 0, 0, 0], 0, 3) == 0)
+# print(subarray_sum([10, 0, 0, 0], 1, 3) == 0)
+# print(subarray_sum([10, 0, 0, 10], 0, 3) == 10)
+# print(subarray_sum([10, 0, 0, 10], 0, 4) == 20)
+# print(subarray_sum([10, 10, 10, 10], 1, 3) == 20)
