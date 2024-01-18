d6ad9889560363f95b170a6cff1786e1d10a6c23
abed42 <abedhamami42@gmail.com>
2024-01-18 15:08:24 +0200

    Implemented a function to calculate the minimum number of deletions required to make a string of parentheses valid.

---
+++ src/brain_teasers/python/deletions_to_make_valid_parentheses/src/deletions_to_make_valid_parentheses.py
@@ -0,0 +1,24 @@
+def deletions_to_make_valid_parentheses(input_string):
+   """
+   Calculate the minimum number of deletions required to balance parentheses in a given input string.
+
+   Parameters:
+       input_string (str): The input string containing parentheses.
+
+   Returns:
+       int: The minimum number of deletions required to balance the parentheses.
+   """
+   stack = []
+   deletions = 0
+
+   for char in input_string:
+       if char == '(':
+           stack.append(char)
+       elif char == ')':
+           if not stack:
+               deletions += 1
+           else:
+               stack.pop()
+
+   deletions += len(stack)
+   return deletions
