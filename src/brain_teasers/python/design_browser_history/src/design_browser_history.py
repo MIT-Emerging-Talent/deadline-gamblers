812a321b105e4e69845aacea7024edd378d03f80
abed42 <abedhamami42@gmail.com>
2024-01-19 15:58:05 +0200

    added documentation to readme file and formatted with black

--- src/brain_teasers/python/design_browser_history/src/design_browser_history.py
+++ src/brain_teasers/python/design_browser_history/src/design_browser_history.py
@@ -1,50 +1,59 @@
+"""
+Module Name: design_browser_history
+Description: This module provides a class to manage browser history.
+Author: Abed Hamami
+"""
+
+
 class BrowserHistory:
-   def __init__(self, start_url):
-       """
-       Initializes a new instance of the class.
+    def __init__(self, start_url):
+        """
+        Initializes a new instance of the class.
 
-       Args:
-           start_url (str): The starting URL of the instance.
-       """
-       self.history = [start_url]
-       self.current_position = 0
+        Args:
+            start_url (str): The starting URL of the instance.
+        """
+        self.history = [start_url]
+        self.current_position = 0
 
-   def visit(self, url):
-       """
-       Updates the browser history with a new URL.
+    def visit(self, url):
+        """
+        Updates the browser history with a new URL.
 
-       Args:
-           url (str): The URL to be added to the browser history.
+        Args:
+            url (str): The URL to be added to the browser history.
 
-       Returns:
-           None
-       """
-       self.history = self.history[:self.current_position + 1]
-       self.history.append(url)
-       self.current_position += 1
+        Returns:
+            None
+        """
+        self.history = self.history[: self.current_position + 1]
+        self.history.append(url)
+        self.current_position += 1
 
-   def back(self, steps):
-       """
-       Decreases the current position in the history list by the specified number of steps.
+    def back(self, steps):
+        """
+        Decreases the current position in the history list by the specified number of steps.
 
-       Parameters:
-           - steps (int): The number of steps to move backwards in the history list.
+        Parameters:
+            - steps (int): The number of steps to move backwards in the history list.
 
-       Returns:
-           - The item in the history list at the updated current position.
-       """
-       self.current_position = max(self.current_position - steps, 0)
-       return self.history[self.current_position]
+        Returns:
+            - The item in the history list at the updated current position.
+        """
+        self.current_position = max(self.current_position - steps, 0)
+        return self.history[self.current_position]
 
-   def forward(self, steps):
-       """
-       Move the current position forward by the specified number of steps.
+    def forward(self, steps):
+        """
+        Move the current position forward by the specified number of steps.
 
-       Parameters:
-           steps (int): The number of steps to move forward.
+        Parameters:
+            steps (int): The number of steps to move forward.
 
-       Returns:
-           The element at the updated current position in the history.
-       """
-       self.current_position = min(self.current_position + steps, len(self.history) - 1)
-       return self.history[self.current_position]
+        Returns:
+            The element at the updated current position in the history.
+        """
+        self.current_position = min(
+            self.current_position + steps, len(self.history) - 1
+        )
+        return self.history[self.current_position]
