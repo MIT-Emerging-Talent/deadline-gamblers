ba9966159f6491298f83502772975d27d2240734
adel-batal <batal.adel24@gmail.com>
2024-01-19 18:00:55 +0200

    Imporve DFS algorithm #28

--- src/graphs/python/dfs/src/dfs.py
+++ src/graphs/python/dfs/src/dfs.py
@@ -5,12 +5,13 @@ def dfs(graph, current_vertex, target_vertex, visited=None):
     visited.add(current_vertex)
 
     if current_vertex == target_vertex:
-        return True
+        return 0
 
     for edge in graph.edges_from_vertex(current_vertex):
         neighbor_vertex = edge.destination
         if neighbor_vertex not in visited:
-            if dfs(graph, neighbor_vertex, target_vertex, visited):
-                return True
+            distance_sum = dfs(graph, neighbor_vertex, target_vertex, visited)
+            if distance_sum:
+                return distance_sum + edge.distance
 
-    return False
+    return float('inf')
