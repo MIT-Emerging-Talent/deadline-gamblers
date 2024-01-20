96fd7ad383c288616f6ab9037c6752bede35ee09
adel-batal <batal.adel24@gmail.com>
2024-01-20 23:24:00 +0200

    Add check to handle the case when vertex does not exist in graph

--- src/graphs/python/dfs/src/dfs.py
+++ src/graphs/python/dfs/src/dfs.py
@@ -1,17 +1,22 @@
-def dfs(graph, current_vertex, target_vertex, visited=None):
+def dfs(graph, current_vertex, target_vertex, visited=None, distance=0):
     if visited is None:
         visited = set()
+        
+    if not graph.contains(current_vertex):
+        return float('inf')
+
 
     visited.add(current_vertex)
 
     if current_vertex == target_vertex:
-        return 0
+        return distance
+
+    min_distance = float('inf')
 
     for edge in graph.edges_from_vertex(current_vertex):
         neighbor_vertex = edge.destination
         if neighbor_vertex not in visited:
-            distance_sum = dfs(graph, neighbor_vertex, target_vertex, visited)
-            if distance_sum:
-                return distance_sum + edge.distance
-
-    return float('inf')
+            current_distance = dfs(graph, neighbor_vertex, target_vertex, visited, distance + edge.distance)
+            if current_distance < min_distance:
+                min_distance = current_distance
+    return min_distance
