366f76034d8839874d22490b9e6e8c14e27ca348
adel-batal <batal.adel24@gmail.com>
2024-01-21 00:37:42 +0200

    Implement fibonacci

--- src/dynamic_programming/python/fibonacci/src/fib.py
+++ src/dynamic_programming/python/fibonacci/src/fib.py
@@ -1,31 +1,29 @@
-def fib_basic(n):
-    if n <= 0:
-        return 0
-    elif n == 1:
-        return 1
+def fib(n: int) -> int:
+    """
+    Calculate the nth Fibonacci number using a recursive approach.
+
+    Args:
+    - n (int): The position in the Fibonacci sequence to calculate. The first position is 0.
+
+    Returns:
+    - int: The nth Fibonacci number.
+
+    Raises:
+    - ValueError: If n is negative.
+
+    Examples:
+    >>> fib(0)
+    0
+    >>> fib(1)
+    1
+    >>> fib(5)
+    5
+    >>> fib(10)
+    55
+    """
+    if n < 0:
+        raise ValueError("n must be a non-negative integer")
+    elif n <= 1:
+        return n
     else:
-        return fib_basic(n - 2) + fib_basic(n - 1)
-
-
-def fib_memo(n, memo=dict()):
-    if n < 1:
-        return 0
-    if n <= 2:
-        return 1
-    return memo.get(n, fib_memo(n - 1, memo) + fib_memo(n - 2, memo))
-
-
-def fib_tab(n):
-    if n < 1:
-        return 0
-
-    if n == 1:
-        return 1
-
-    table = [0] * (n + 1)
-    table[1] = 1
-
-    for i in range(2, n + 1):
-        table[i] = table[i - 1] + table[i - 2]
-
-    return table[n]
+        return fib(n - 1) + fib(n - 2)
